51186df3f5ff5e4a67dfdf2f974d5823beb5dfce
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2023-03-12 21:19:33 +0300

    6

--- src/js/Validator/Validator.js
+++ src/js/Validator/Validator.js
@@ -1,14 +1,18 @@
 export default class Validator {
   constructor(userName) {
     this.userName = userName;
+    this.noLettersDashUnderscoreStart = /^[^\d_-]/;
+    this.noLettersDashUnderscoreEnd = /[^\d_-]$/;
+    this.exeptThreeNumbersInRow = !/\d{4,}/;
+    this.onlyEnglishLetter = /[\w\-_]+/;
   }
 
   validateUsername() {
-    const exeptThreeNumbersInRow = !/\d{4,}/;
-    const onlyEnglishLetter = /[\w\-\_]+/;
-    const noLettersDashUnderscoreStart = /^[^\d\_\-]/;
-    const noLettersDashUnderscoreEnd = /[^\d\_\-]$/;
-    return (noLettersDashUnderscoreStart && onlyEnglishLetter && noLettersDashUnderscoreEnd && exeptThreeNumbersInRow);
+    return this.noLettersDashUnderscoreStart.test(this.userName);
+    // ни как не могу понять почему один this.noLettersDashUnderscoreStart.test(this.userName) работает, а когда два условия, например,
+    // return (this.noLettersDashUnderscoreStart && this.noLettersDashUnderscoreEnd).test(this.userName) - одно из условий перестает работать...
+    // а остальные 2 вообще не работают..) Помогите, пожалуйста, разобраться что не так... также lint ругается, что строка return 
+    // получается длиннее чем 130 символов, что нужно сделать в этом случае?
 
     // return (
     //   /^[^\d\_\-][\w\-\_]+[^\d\_\-]$/.test(this.userName) &&
