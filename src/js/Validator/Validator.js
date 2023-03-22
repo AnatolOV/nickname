6c5a549bab7156d0d9d1395a463facd6ad7d9a02
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2023-03-22 11:30:21 +0300

    31

--- src/js/Validator/Validator.js
+++ src/js/Validator/Validator.js
@@ -3,25 +3,16 @@ export default class Validator {
     this.userName = userName;
     this.noLettersDashUnderscoreStart = /^[^\d_-]/;
     this.noLettersDashUnderscoreEnd = /[^\d_-]$/;
-    this.exeptThreeNumbersInRow = !/\d\d\d+/;
+    this.threeNumbersInRow = /\d{4,}/;
     this.onlyEnglishLetter = /[\w\-_]+/;
   }
 
   validateUsername() {
-    // пробую делать в соответствии с комментарием, не работает(см.ниже)
-    // пробую выполнять частями, тоже не работают, например return this.exeptThreeNumbersInRow.test(this.userName) - не работает, а если
-    //  запускаю return !/\d\d\d+/.test(this.userName) - работает 
-
     return (
-      this.noLettersDashUnderscoreStart.test(this.userName) &&
-      this.onlyEnglishLetter.test(this.userName) &&
-      this.noLettersDashUnderscoreEnd.test(this.userName) &&
-      this.exeptThreeNumbersInRow.test(this.userName)
+      this.noLettersDashUnderscoreStart.test(this.userName)
+      && this.onlyEnglishLetter.test(this.userName)
+      && this.noLettersDashUnderscoreEnd.test(this.userName)
+      && !this.threeNumbersInRow.test(this.userName)
     );
-
-    // return (
-    //   /^[^\d\_\-][\w\-\_]+[^\d\_\-]$/.test(this.userName) &&
-    //   !/\d\d\d+/.test(this.userName)
-    // );
   }
 }
--- src/js/__tests__/Validator.test.js
+++ src/js/__tests__/Validator.test.js
@@ -1,7 +1,7 @@
 import Validator from '../Validator/Validator';
 
 test('Валидация username', () => {
-  const input = new Validator('eeerr').validateUsername();
+  const input = new Validator('ddd522ddd').validateUsername();
 
   expect(input).toEqual(true);
 });
